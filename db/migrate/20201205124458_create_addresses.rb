658adff2ab0b0a9eee4aa2c652165ae5c988813e
ED1FF <alekarp11@gmail.com>
2020-12-05 15:01:35 +0200

    Add cities and address models

---
+++ db/migrate/20201205124458_create_addresses.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class CreateAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addresses do |t|
+      t.refereces :city, index: true
+      t.string :street
+      t.bigint  :addressable_id
+      t.string  :addressable_type
+      t.string :notes
+      t.string :phone_number
+
+      t.timestamps
+    end
+    add_index :addresses, %i[addressable_type addressable_id]
+  end
+end
